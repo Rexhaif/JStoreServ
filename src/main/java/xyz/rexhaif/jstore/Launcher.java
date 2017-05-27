fe3476f7a6dfe292b512abad67b92aa086484cb1
Rexhaif <larionov.messerschmitt@gmail.com>
2017-05-27 22:31:17 +0300

    license

--- src/main/java/xyz/rexhaif/jstore/Launcher.java
+++ src/main/java/xyz/rexhaif/jstore/Launcher.java
@@ -5,8 +5,11 @@ import io.vertx.core.Vertx;
 public class Launcher {
 
     public static void main(String[] args) {
+        System.err.println("Running StoreServ node");
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new ApiVerticle());
+        vertx.deployVerticle(new ApiVerticle(), res -> {
+            System.err.println("StoreServ node Deployed");
+        });
     }
 
 }
